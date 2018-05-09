28ce1df8f9e6729a490a039f4ad41029602c6fb0
madzia89 <wojcicka.ma@gmail.com>
2018-05-09 12:31:04 +0200

    add text field, save input at firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
 
 
 class App extends Component {
 
     state = {
-        counter: 0
+        counter: 0,
+        newText: ''
     }
 
 
     componentDidMount() {
         this.readFromDb()
+        this.readFromDbs()
     }
 
     readFromDb = () => {
@@ -20,6 +24,15 @@ class App extends Component {
                 })
             )
     }
+    readFromDbs = () => {
+        fetch('https://jfddl4-sandbox.firebaseio.com/magda/text/.json')
+            .then(response => response.json())
+            .then(actualText => this.setState({
+                    newText: actualText
+                })
+            )
+    }
+
 
     saveToDb = (data) => fetch('https://jfddl4-sandbox.firebaseio.com/magda/counter/.json',
         {
@@ -27,6 +40,13 @@ class App extends Component {
             body: JSON.stringify(data)
         }
     ).then(this.readFromDb)
+
+    saveToDbs = (text) => fetch('https://jfddl4-sandbox.firebaseio.com/magda/text/.json',
+        {
+            method: "PUT",
+            body: JSON.stringify(text)
+        }
+    ).then(this.readFromDbs)
 
 
     decHandler = () => {
@@ -37,6 +57,17 @@ class App extends Component {
         this.saveToDb(this.state.counter + 1)
     }
 
+    addText =() => {
+        this.saveToDbs(
+            this.state.newText
+        )
+    }
+
+    newText = (event, newText) => {
+        this.setState({
+            newText: newText
+        })
+    }
 
     render() {
         return (
@@ -45,6 +76,21 @@ class App extends Component {
                 <h2>{this.state.counter}</h2>
                 <button onClick={this.decHandler}>-</button>
                 <button onClick={this.incHandler}>+</button>
+                <div>
+                    <TextField
+                        onChange={this.newText}
+                        value={this.state.newText}
+                        name={'new-tet'}
+                        placeholder={'Add new text'}
+                        fullWidth={true}
+                    />
+                    <RaisedButton
+                        onClick={this.addText}
+                        primary={true}
+                        fullWidth={true}
+                        label={'ADD'}
+                    />
+                </div>
             </div>
         )
     }
