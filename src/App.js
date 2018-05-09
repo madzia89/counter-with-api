e577425be63d758ac0388940c4299079f785827b
madzia89 <wojcicka.ma@gmail.com>
2018-05-09 11:07:49 +0200

    add buttons and functions to count

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-<h1>Da kaunter</h1>
-      </div>
-    )
-  }
+
+        state = {
+            counter: 0
+        }
+
+        decHandler = () => {
+            this.setState({
+                counter: this.state.counter - 1
+            })
+        }
+
+        incHandler = () => {
+            this.setState({
+                counter: this.state.counter + 1
+            })
+        }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.counter}</h1>
+                <button onClick={this.decHandler}>-</button>
+                <button onClick={this.incHandler}>+</button>
+            </div>
+        )
+    }
 }
+
 
 export default App;
