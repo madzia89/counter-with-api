db3f49aaa2a7e88de525633883591846fc3ac4ab
madzia89 <wojcicka.ma@gmail.com>
2018-05-09 11:36:46 +0200

    state reacting with api

--- src/App.js
+++ src/App.js
@@ -3,25 +3,44 @@ import React, {Component} from 'react';
 
 class App extends Component {
 
-        state = {
-            counter: 0
-        }
+    state = {
+        counter: ''
+    }
 
-        decHandler = () => {
-            this.setState({
-                counter: this.state.counter - 1
-            })
-        }
+    decHandler = () => {
+        this.setState({
+            counter: this.state.counter - 1
+        }, this.saveToDb)
+    }
 
-        incHandler = () => {
-            this.setState({
-                counter: this.state.counter + 1
-            })
+    incHandler = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        }, this.saveToDb)
+    }
+
+    componentDidMount = () => {
+        fetch('https://jfddl4-sandbox.firebaseio.com/magda/counter/.json')
+            .then(response => response.json())
+            .then(actualCounterVal => this.setState({
+                    counter: actualCounterVal
+                })
+            )
+    }
+
+    saveToDb = () => fetch('https://jfddl4-sandbox.firebaseio.com/magda/counter/.json',
+        {
+            method: "PUT",
+            body: JSON.stringify(this.state.counter)
         }
+    )
+
+
     render() {
         return (
             <div>
-                <h1>{this.state.counter}</h1>
+                <h2>Da kaunter</h2>
+                <h2>{this.state.counter}</h2>
                 <button onClick={this.decHandler}>-</button>
                 <button onClick={this.incHandler}>+</button>
             </div>
